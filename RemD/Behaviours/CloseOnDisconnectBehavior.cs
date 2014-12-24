c70b646944d5584ea9dda55b13e286bcaed92c2f
Mattias Cibien <mattias@mattiascibien.net>
2014-12-24 15:59:41 +0100

    Initial close on disconnect.

---
+++ RemD/Behaviours/CloseOnDisconnectBehavior.cs
@@ -0,0 +1,39 @@
+﻿using RemD.Interop;
+using RemD.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace RemD.Behaviours
+{
+    public class CommandOnDisconnectBehavior : Behavior<WindowsFormsHost>
+    {
+        private RDPControl RDPControl;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            RDPControl = (AssociatedObject.Child as RDPControl);
+
+            RDPControl.ActiveX.OnDisconnected += OnDisconnected;
+        }
+
+        void OnDisconnected(object sender, AxMSTSCLib.IMsTscAxEvents_OnDisconnectedEvent e)
+        {
+            
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            RDPControl.ActiveX.OnDisconnected -= OnDisconnected;
+        }
+    }
+}
